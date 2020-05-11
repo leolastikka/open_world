26959278a48d10ec5b1e676b5fe96f4ca2095d65
Leo Lastikka <leo.lastikka@gmail.com>
2020-05-11 23:15:22 +0300

    Fix mouse position again

--- client/js/display.js
+++ client/js/display.js
@@ -79,7 +79,7 @@ class Display {
     const pos = new Vector2(tileX, tileY);
     pos.div(this.zoomLevel);
     pos.add(this.pos);
-    pos.add(new Vector2(-2, -1.5)); // fix something
+    pos.add(new Vector2(-2, -2)); // fix something
     return pos;
   }
 
